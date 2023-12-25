098599254ac54ef999d61c5b57f0acb3b6ca0eef
narasimha-1511 <s.narasimha.2005@gmail.com>
2023-12-25 16:06:03 +0530

    added borrowing feature

--- controller/Book.go
+++ controller/Book.go
@@ -1,9 +1,13 @@
 package controller
 
 import (
+	"fmt"
 	"math/rand"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/narasimha-1511/zolo-backend/config"
 	"github.com/narasimha-1511/zolo-backend/models"
 )
@@ -26,7 +30,6 @@ func CreateBookParams(c *gin.Context){
     book.Name = c.Param("name")
     book.Title = c.Param("title")
     book.Author = c.Param("author")
-    book.Quantity = 1
 
 	config.DB.AutoMigrate(&models.Book{})
 
@@ -44,11 +47,98 @@ func CreateBookPostForm(c *gin.Context){
 	book.Name = c.PostForm("name")
 	book.Title = c.PostForm("title")
 	book.Author = c.PostForm("author")
-	book.Quantity = 1
 
 	config.DB.Create(&book)
 
 	c.JSON(200, gin.H{
 		"data": book,
 	})
+}
+
+func BorrowBook(c *gin.Context){
+
+	var book models.Book
+
+	book_id := c.Param("id")
+
+	result := config.DB.Where("book_id = ?", book_id).First(&book)
+	if result.Error != nil {
+    fmt.Println(result.Error)
+	}
+
+	//Check if the book is already borrowed
+	if book.Borrowed == true {
+		c.JSON(200, gin.H{
+			"message": "Book is already borrowed",
+		})	
+		return
+	}
+
+	config.DB.Model(&book).Update("borrowed", true)
+
+	var borrowed models.Borrowed
+
+	borrowed.BorrowedID = uuid.New()
+
+	time_needed_str := c.PostForm("borrow_period") // this is in days
+
+	if time_needed_str == "" {
+		time_needed_str = "7" // if no time is given then default is 7 days
+	}
+
+	time_needed, _ := strconv.Atoi(time_needed_str)
+
+	borrowed.BookID = book.BookID
+	borrowed.StartTime = time.Now()
+	borrowed.EndTime = time.Now().AddDate(0, 0, time_needed)
+
+	config.DB.Create(&borrowed)
+
+	c.JSON(200, gin.H{
+		"data": book,
+		"boorowd": borrowed,
+	})
+}
+
+func GetBorrowedBooks(c *gin.Context){
+	var borrowed []models.Borrowed
+
+	result := config.DB.Find(&borrowed)
+
+	if result.Error != nil {
+		// Handle error
+		fmt.Println(result.Error)
+	}
+
+	c.JSON(200, gin.H{
+		"Borrowed_Data": borrowed,
+	})
+
+}
+
+func ReturnBook(c *gin.Context){
+
+	var borrowed models.Borrowed
+	var book models.Book
+
+	book_id := c.Param("book_id")
+	borrow_id := c.Param("borrow_id")
+
+	result := config.DB.Where("book_id = ?", book_id).First(&book)
+	if result.Error != nil {
+    fmt.Println(result.Error)
+	}
+
+	result = config.DB.Where("borrowed_id = ?", borrow_id).First(&borrowed)
+	if result.Error != nil {
+	fmt.Println(result.Error)
+	}
+
+	config.DB.Model(&book).Update("borrowed", false)
+	config.DB.Model(&borrowed).Update("returned", true)
+
+	c.JSON(200, gin.H{
+		"message": "Book Returned , Thank you for using our service",
+	})
+
 }
--- models/book.go
+++ models/book.go
@@ -1,15 +1,26 @@
 package models
 
 import (
+	"time"
+
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Book struct {
     gorm.Model
-    BookID   uint64 `gorm:"type:bigint"`
+    BookID   uint64 `json:"book_id" gorm:"type:bigint"`
     Name     string `json:"name"`
     Title    string `json:"title"`
-    Author   string `json:"author"`
-    Quantity int    `json:"quantity"`
+    Author   string  `json:"author"`
     Borrowed bool   `json:"borrowed" gorm:"default:false"`
+}
+
+type Borrowed struct {
+    gorm.Model
+    BorrowedID  uuid.UUID   `json:"borrowed_id" gorm:"type:uuid;"`
+    BookID      uint64   `json:"book_id" gorm:"type:bigint"`
+    StartTime   time.Time     `json:"startTimestamp"`
+    EndTime     time.Time     `json:"endTimestamp"`
+    Returned    bool       `json:"returned" gorm:"default:false"`
 }
