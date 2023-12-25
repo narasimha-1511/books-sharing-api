e97c5b9699ac6c84b5d301226bfd5b26474c1b31
narasimha-1511 <s.narasimha.2005@gmail.com>
2023-12-25 14:46:04 +0530

    setting up databse

---
+++ config/db.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/narasimha-1511/zolo-backend/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect(){
+	// Do something here.
+	// db, err := gorm.Open(postgres.Open("postgres://postgres:postgres@localhost:5432/postgres"))
+
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connection Opened to Database")
+
+	db.AutoMigrate(&models.Book{})
+	
+	DB = db
+
+}
